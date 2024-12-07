7266224d656056526c8188a9b2a3c9f4458ced53
Jan Stusio <jan.stusio@protonmail.com>
2024-12-07 18:02:21 +0100

    init xcode proj

---
+++ SkySense/SkySense/SkySenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkySenseApp.swift
+//  SkySense
+//
+//  Created by Jan Stusio on 07/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SkySenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
